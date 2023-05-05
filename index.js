5fcce3a8c32523a48313d0494f7ca4fdad4397d5
Тайсумова Оксана <119331948+Oksana2211@users.noreply.github.com>
2023-05-05 10:03:20 +0300

    correction

--- index.js
+++ index.js
@@ -23,41 +23,43 @@ blockTasksHeader2.textContent = "Список задач";
 blockTasks.appendChild(blockTasksHeader2);//добавляем заголовок2 в основном блок (section)
 
 
-
-
 const сonclusion = document.createElement('div'); //создаем поле для вывода
 сonclusion.className = 'block-tasks__сonclusion';
 blockTasks.appendChild(сonclusion);//добавляем поле в основном блок (section)
 
 
-const inputNoTasks = document.createElement('span');// "Нет задач"
+const inputNoTasks = document.createElement('span');// надпись "Нет задач"
 inputNoTasks.className = 'no-tasks';
 inputNoTasks.textContent = "Нет задач";
-сonclusion.appendChild(inputNoTasks);
+сonclusion.appendChild(inputNoTasks);// добавляем элемент в div - поле для вывода
 
 
-const cleaningBtn = document.createElement('button');//создаем кнопку для добавления задач
+const cleaningBtn = document.createElement('button');//создаем кнопку для удаления задач
 cleaningBtn.className = 'block-tasks__cleaningBtn';
 cleaningBtn.textContent = "Очистить список задач";
 blockTasks.appendChild(cleaningBtn);//добавляем кнопку в основном блок (section)
 
-
+// функция при нажатии на кнопку "Добавить"
 inputBtn.addEventListener('click', function addTask() {
 
+
   inputNoTasks.remove();//удаляем "Нет задач"
+  cleaningBtn.removeAttribute('disabled'); //кнопка "очистить список задач" активна
+  cleaningBtn.classList.add('block-tasks__cleaningBtn');// стилизуем активную кнопку
+
 
   const taskList = document.createElement('ul');//контейнер для задач
   сonclusion.appendChild(taskList);//добавляем контейнер для задач в div
-  const node = document.createElement("li");
+  const node = document.createElement("li");//контейнер для записи каждой задачи
 
 
   //создаем строку с задачей и добовляем ее в контейнер для задач
   node.innerHTML += `${input.value}` + '<span class="close">\u00D7</span>';
   input.value = '';
-  taskList.appendChild(node);
+  taskList.appendChild(node);//добавляем задачу в LI-контейнер
 
 
-  //делаем активным крестик у элемента
+  //делаем активным крестик у каждой задачи
   const close = document.getElementsByClassName("close");
   for (let i = 0; i < close.length; i++) {
     close[i].onclick = function () {
@@ -76,9 +78,10 @@ inputBtn.addEventListener('click', function addTask() {
 })
 
 
-
-
+// функция при нажатии на кнопку "очистить список задач"
 cleaningBtn.addEventListener('click', function cleanTaskList() {
-  сonclusion.textContent = "";
-  сonclusion.appendChild(inputNoTasks);
+  сonclusion.textContent = ""; //очищаем div-контейнер от задач
+  сonclusion.appendChild(inputNoTasks);// "Нет задач" добавляем надпись в div
+  cleaningBtn.setAttribute('disabled', true);//кнопка "очистить список задач" не активна
+  cleaningBtn.className = 'btn:disabled';//стилизуем неактивную кнопку
 })
